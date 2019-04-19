c1bc535aa87ab50031eefa5706abe7971fe26b05
Syed Farhan <42010556+born-2learn@users.noreply.github.com>
2019-04-19 15:45:57 +0530

    Updated fifo.v

--- verilog-codes/fifo.v
+++ verilog-codes/fifo.v
@@ -1,20 +1,32 @@
 //FIFO(first in first out)
 
 module fifodesign(clk,rst,wr,rd,din,dout,empty,full);
-input clk,rst; input wr,rd; input [3:0] din;
+input clk,rst; 
+input wr,rd; 
+input [3:0] din;
+  
 output reg [3:0] dout;
-output empty,full; reg [3:0] mem [0:7]; wire [3:0] wrptr,rdptr;
-reg [3:0] bwrptr,brdptr; always@(posedge clk) begin
+output empty,full; 
+  
+reg [3:0] mem [0:7];
+wire [3:0] wrptr,rdptr;
+reg [3:0] bwrptr,brdptr; 
+  
+always@(posedge clk) begin
 if(wr)
 mem[wrptr] <= din;
 end
+  
 always@(posedge clk)
 begin if(rd)
 dout <= mem[rdptr];
 end
+  
 assign empty = wrptr == rdptr;
 assign full = ((wrptr[3] != rdptr[3]) && (wrptr[2] != rdptr[2]) &&(wrptr[1:0] ==
-rdptr[1:0])); always@(posedge clk) begin
+rdptr[1:0]));
+  
+always@(posedge clk) begin
 if(rst)
 begin
 bwrptr <=0; brdptr <=0; end
